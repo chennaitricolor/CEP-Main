01e39e47f55ad17e9dbdfd60a482ac0c3d31d1c2
Thilagarajan Kamarajan <kthilagarajan@gmail.com>
2019-03-04 11:13:50 +0530

    Splash Screen Added

--- lib/routes/splash/splash.dart
+++ lib/routes/splash/splash.dart
@@ -32,13 +32,113 @@ class SplashState extends State<Splash> {
   @override
   void initState() {
     super.initState();
-    startTime();
+    // startTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.white),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 100.0,
+                        child: Image(
+                          image: AssetImage(
+                              'assets/images/logo/techforcities.png'),
+                          width: 150.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                      ),
+                      Text(
+                        "App Title",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0),
+                      ),
+                      Container(
+                        width: 50,
+                        height: 50,
+                        child: Divider(
+                          color: Colors.blueAccent,
+                          height: 30,
+                        ),
+                      ),
+                      Text(
+                        "Here we have Tag Line",
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Image(
+                image: AssetImage(
+                    'assets/images/logo/splash_bg.png'),
+                width: MediaQuery.of(context).size.width,
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                    ),
+                    CircularProgressIndicator(
+                      valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                    ),
+                    Text(
+                      "Footer Text 1",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                            fontSize: 16.0),
+                    ),
+                    Text(
+                      "This is Footer Text 2",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.grey,
+                            fontSize: 14.0),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                    ),
+                  ],
+                ),
+                )
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
